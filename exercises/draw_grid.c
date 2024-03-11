2a8e73f3edecaaa05e1536d7b435118450d1051d
loki0b <riqc0nt4ct@hotmail.com>
2024-03-10 23:38:40 -0300

    add exercises header file and fisr exercise draw_grid

---
+++ exercises/draw_grid.c
@@ -0,0 +1,22 @@
+#include <SDL2/SDL.h>
+#include <stdint.h>
+
+extern uint32_t* color_buffer;
+extern int window_width, window_height;
+
+void draw_grid(void) {
+    uint32_t color = 0xFF000000;
+
+    for (int h = 0; h < window_height; h++) {
+        if (h % 10 == 0) {
+	    for (int w = 0; w < window_width; w++) {
+	        color_buffer[(window_width * h) + w] = color;
+	    }
+	}
+        else {
+	    for (int w = 0; w < window_width; w += 10) {
+	        color_buffer[(window_width * h) + w] = color;
+	    }
+	}
+    }
+}
---
+++ exercises/exercises.h
@@ -0,0 +1,6 @@
+#ifndef _EXERCISES_H
+#define _EXERCISES_H
+
+void draw_grid(void);
+
+#endif
