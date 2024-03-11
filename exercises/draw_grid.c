5f55f81047d25b391f83ce805ea9e044bc9162ca
loki0b <riqc0nt4ct@hotmail.com>
2024-03-11 00:03:33 -0300

    add a dot grid and update function names

--- exercises/draw_grid.c
+++ exercises/draw_grid.c
@@ -4,9 +4,9 @@
 extern uint32_t* color_buffer;
 extern int window_width, window_height;
 
-void draw_grid(void) {
-    uint32_t color = 0xFF000000;
+uint32_t color = 0xFF333333;
 
+void draw_line_grid(void) {
     for (int h = 0; h < window_height; h++) {
         if (h % 10 == 0) {
 	        for (int w = 0; w < window_width; w++) {
@@ -19,4 +19,12 @@ void draw_grid(void) {
 	        }
         }
     }
+}
+
+void draw_dot_grid(void) {
+    for (int h = 0; h < window_height; h += 10) {
+        for (int w = 0; w < window_width; w += 10) {
+            color_buffer[(window_width * h) + w] = color;
+        }
+    } 
 }
--- exercises/exercises.h
+++ exercises/exercises.h
@@ -1,6 +1,7 @@
 #ifndef _EXERCISES_H
 #define _EXERCISES_H
 
-void draw_grid(void);
+void draw_line_grid(void);
+void draw_dot_gird(void);
 
 #endif
