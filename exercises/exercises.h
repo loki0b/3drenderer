0594280c1bf298491a02553d440c5a4f07e0b3e3
loki0b <riqc0nt4ct@hotmail.com>
2024-03-16 00:20:23 -0300

    add draw_rect function

--- exercises/exercises.h
+++ exercises/exercises.h
@@ -1,7 +1,10 @@
+#include <stdint.h>
+
 #ifndef _EXERCISES_H
 #define _EXERCISES_H
 
 void draw_line_grid(void);
 void draw_dot_gird(void);
+void draw_rect(int x, int y, int width, int height, uint32_t color);
 
 #endif
